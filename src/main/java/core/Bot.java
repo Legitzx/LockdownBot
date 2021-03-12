3954d07f506bdb17408d28122be3e6078972315e
lk <luciano.kho18@gmail.com>
2021-03-11 20:34:19 -0800

    Added DatabaseApi.

--- src/main/java/core/Bot.java
+++ src/main/java/core/Bot.java
@@ -1,24 +1,37 @@
 package core;
 
 import database.DatabaseApi;
+import models.LockdownServer;
+import models.LockdownServerConverter;
+import models.LockdownUserConverter;
 import net.dv8tion.jda.api.JDABuilder;
 import utils.Config;
 
 import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Contributor(s): Luciano K
  * Description: Main class
  */
 public class Bot {
-    // Dependencies
+    // Config
     private Config config;
+
+    // APIs
     private DatabaseApi databaseApi;
+
+    // Converters
+    private LockdownServerConverter serverConverter;
+    private LockdownUserConverter userConverter;
 
     public Bot() throws LoginException {
         // Initialize Dependencies
         config = new Config();
         databaseApi = new DatabaseApi(this);
+        serverConverter = new LockdownServerConverter();
+        userConverter = new LockdownUserConverter();
 
         JDABuilder builder = JDABuilder.createLight(config.get("TOKEN"));
         builder.build();
@@ -30,6 +43,14 @@ public class Bot {
 
     public DatabaseApi getDatabaseApi() {
         return databaseApi;
+    }
+
+    public LockdownServerConverter getServerConverter() {
+        return serverConverter;
+    }
+
+    public LockdownUserConverter getUserConverter() {
+        return userConverter;
     }
 
     public static void main(String[] args) throws LoginException {
--- src/main/java/database/DatabaseApi.java
+++ src/main/java/database/DatabaseApi.java
@@ -1,18 +1,92 @@
 package database;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.lang.Nullable;
 import core.Bot;
+import models.LockdownServer;
+import models.LockdownUser;
+import org.bson.Document;
 
 /**
  * Contributor(s): Luciano K
- * Description:
+ * Description: Interacts with MongoDB
  */
 public class DatabaseApi extends DatabaseConnection {
+    // Main bot
+    private Bot bot;
+
     // Gets main collection
-    private MongoCollection collection;
+    private MongoCollection serverCollection;
+    private MongoCollection userCollection;
 
     public DatabaseApi(Bot bot) {
         super(bot);
-        collection = super.getDatabase().getCollection(bot.getConfig().get("collection"));
+        this.bot = bot;
+        serverCollection = super.getDatabase().getCollection(bot.getConfig().get("server_collection"));
+        userCollection = super.getDatabase().getCollection(bot.getConfig().get("user_collection"));
+    }
+
+    /**
+     * Creates a LockdownUser in the MongoDB.
+     * @param user      User you wish to create.
+     */
+    public void createLockdownUser(LockdownUser user) {
+        userCollection.insertOne(new Document(bot.getUserConverter().serialize(user)));
+    }
+
+    /**
+     * Creates a LockdownServer in the MongoDB.
+     * @param server
+     */
+    public void createLockdownServer(LockdownServer server) {
+        serverCollection.insertOne(new Document(bot.getServerConverter().serialize(server)));
+    }
+
+    /**
+     * Gets a LockdownServer from the MongoDB.
+     * @param id        ID of the server you wish to get.
+     * @return          LockdownServer or null (if server does not exist).
+     */
+    @Nullable
+    public LockdownServer getLockdownServer(String id) {
+        try {
+            Document found = (Document) serverCollection.find(new Document("id", id)).first();
+
+            return bot.getServerConverter().deserialize(found);
+        } catch (NullPointerException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Gets a LockdownUser from the MongoDB.
+     * @param id        ID of the user you wish to get.
+     * @return          LockdownUser or null (if user does not exist).
+     */
+    @Nullable
+    public LockdownUser getLockdownUser(String id) {
+        try {
+            Document found = (Document) userCollection.find(new Document("id", id)).first();
+
+            return bot.getUserConverter().deserialize(found);
+        } catch (NullPointerException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Updates a LockdownUser.
+     * @param user      User you wish to update.
+     */
+    public void updateLockdownUser(LockdownUser user) {
+        userCollection.findOneAndUpdate(new Document("id", user.getId()), new Document("$set", bot.getUserConverter().serialize(user)));
+    }
+
+    /**
+     * Updates a LockdownServer.
+     * @param server    Server you wish to update.
+     */
+    public void updateLockdownServer(LockdownServer server) {
+        serverCollection.findOneAndUpdate(new Document("id", server.getId()), new Document("$set", bot.getServerConverter().serialize(server)));
     }
 }
