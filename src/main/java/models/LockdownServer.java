eb9303bad5c1e182829a50ffb26829df6e9cc17b
lk <luciano.kho18@gmail.com>
2021-03-11 19:07:50 -0800

    Added LockdownServer & LockdownServerConverter.

--- src/main/java/models/LockdownServer.java
+++ src/main/java/models/LockdownServer.java
@@ -12,4 +12,52 @@ public class LockdownServer {
     private boolean status; // [locked = false] - [unlocked = true] - This affects the idRole permissions
     private String idLogChannel; // ID of textchannel to log events in
     private String idRole; // ID of the role that will give users access to channels
+
+    public LockdownServer(String id, List<String> authList, boolean status, String idLogChannel, String idRole) {
+        this.id = id;
+        this.authList = authList;
+        this.status = status;
+        this.idLogChannel = idLogChannel;
+        this.idRole = idRole;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<String> getAuthList() {
+        return authList;
+    }
+
+    public void setAuthList(List<String> authList) {
+        this.authList = authList;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getIdLogChannel() {
+        return idLogChannel;
+    }
+
+    public void setIdLogChannel(String idLogChannel) {
+        this.idLogChannel = idLogChannel;
+    }
+
+    public String getIdRole() {
+        return idRole;
+    }
+
+    public void setIdRole(String idRole) {
+        this.idRole = idRole;
+    }
 }
--- src/main/java/models/LockdownServerConverter.java
+++ src/main/java/models/LockdownServerConverter.java
@@ -1,8 +1,43 @@
 package models;
 
+import org.bson.Document;
+
+import java.util.List;
+
 /**
  * Contributor(s): Luciano K
- * Description:
+ * Description: Converts LockdownServer Objects
  */
 public class LockdownServerConverter {
+    /**
+     * Converts MongoDB Documents into LockdownServer Objects.
+     * @param document      Documents you wish to deserialize.
+     * @return              LockdownServer Object.
+     */
+    public LockdownServer deserialize(Document document) {
+        String id = document.getString("id");
+        List<String> authList = document.getList("authList", String.class);
+        boolean status = document.getBoolean("status");
+        String idLogChannel = document.getString("idLogChannel");
+        String idRole = document.getString("idRole");
+
+        return new LockdownServer(id, authList, status, idLogChannel, idRole);
+    }
+
+    /**
+     * Converts LockdownServer Objects into MongoDB Documents.
+     * @param server        Server you wish to serialize.
+     * @return              MongoDB Document comprised of LockdownServer Data.
+     */
+    public Document serialize(LockdownServer server) {
+        Document document = new Document();
+
+        document.put("id", server.getId());
+        document.put("authList", server.getAuthList());
+        document.put("status", server.isStatus());
+        document.put("idLogChannel", server.getIdLogChannel());
+        document.put("idRole", server.getIdRole());
+
+        return document;
+    }
 }
